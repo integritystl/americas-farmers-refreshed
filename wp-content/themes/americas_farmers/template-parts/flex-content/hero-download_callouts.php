e63792b7b4a45a803106979b2d2b3dc4b6bfdb59
Julia Cramer <julia.cramer@integritystl.com>
2020-08-24 10:58:40 -0500

    Tweak for Media Kits

--- wp-content/themes/americas_farmers/template-parts/flex-content/hero-download_callouts.php
+++ wp-content/themes/americas_farmers/template-parts/flex-content/hero-download_callouts.php
@@ -6,38 +6,36 @@ $largeHeadline = get_sub_field('header_large_text');
 ?>
 
 <div class="hero hero-download_callouts hero-striped">
-  <div id="hero-img" class="hero-img-container" data-responsive-background-image>
-    <?php echo wp_get_attachment_image( $heroBgImg, $heroBgImgSize );?>
+  <div id="hero-img" class="hero-img-container" style="background: url('<?php the_sub_field('background_image') ?>')no-repeat center center;; background-size:cover;">
+    <div class="hero-content-container">
+      <?php if (have_rows('striped_header')) { ?>
+        <h1 class="hero-heading">
+          <div class="row">
+          <?php while(have_rows('striped_header')) : the_row();
+            $caps = get_sub_field('all_caps');
+            $lineBreak = get_sub_field('insert_line_break');
+            $bgColor = get_sub_field('background_color');
+            $heroHeadingClass = 'hero-stripe-text';
+
+            //Check if text needs to be caps style or not
+            if ($caps) {
+              $heroHeadingClass .= ' hero-uppercase';
+            }
+            //Get classes for our bg colors
+            if ( $lineBreak ) {
+              $heroHeadingClass .= ' ' . 'line-break';
+            }
+
+          ?>
+            <span class="<?php echo $heroHeadingClass; ?>"><?php the_sub_field('stripe_text'); ?></span>
+          <?php endwhile; ?>
+            </div>
+        </h1>
+
+      <?php }  ?>
+
+    </div>
   </div>
-  <div class="hero-content-container">
-    <?php if (have_rows('striped_header')) { ?>
-      <h1 class="hero-heading">
-        <div class="row">
-        <?php while(have_rows('striped_header')) : the_row();
-          $caps = get_sub_field('all_caps');
-          $lineBreak = get_sub_field('insert_line_break');
-          $bgColor = get_sub_field('background_color');
-          $heroHeadingClass = 'hero-stripe-text';
-
-          //Check if text needs to be caps style or not
-          if ($caps) {
-            $heroHeadingClass .= ' hero-uppercase';
-          }
-          //Get classes for our bg colors
-          if ( $lineBreak ) {
-            $heroHeadingClass .= ' ' . 'line-break';
-          }
-
-        ?>
-          <span class="<?php echo $heroHeadingClass; ?>"><?php the_sub_field('stripe_text'); ?></span>
-        <?php endwhile; ?>
-          </div>
-      </h1>
-
-    <?php }  ?>
-
-  </div>
-
 </div>
 
 <?php if (have_rows('download_items')) { ?>
