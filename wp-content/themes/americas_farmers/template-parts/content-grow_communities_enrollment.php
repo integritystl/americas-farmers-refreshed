706782222bf01bfd0e9d33976b1fbad4273cf972
LauraK <laura.kong@integritystl.com>
2020-08-31 14:08:55 -0700

    update enrollment form fields

--- wp-content/themes/americas_farmers/template-parts/content-grow_communities_enrollment.php
+++ wp-content/themes/americas_farmers/template-parts/content-grow_communities_enrollment.php
@@ -79,15 +79,24 @@ $content = get_field('enroll_content_blurb', $program);
 		<!-- Full Enrollment Form -->
 		<form id="GC_Full_Enrollment_Form" data-parsley-validate="" data-parsley-excluded="input[type=button], input[type=submit], input[type=reset], [disabled]">
 			<fieldset class="enrollment_fieldset">
-				<!-- First Name -->
-				<div class="form-field">
-					<label for="gc_code">Full Name *</label>
-					<div class="field-wrapper">
-						<input type="text" name="full_name" id="full_name" placeholder="Full Name *" data-parsley-trigger="blur" data-parsley-required="a full name" data-parsley-has-special-characters/>
-						<i class="fa fa-exclamation-triangle" aria-hidden="true"></i>
-						<i class="fa fa-check" aria-hidden="true"></i>
-					</div>
-				</div>
+                <!-- First Name -->
+                <div class="form-field">
+                    <label for="gc_code">First Name *</label>
+                    <div class="field-wrapper">
+                        <input type="text" name="first_name" id="first_name" data-parsley-trigger="blur" data-parsley-required="a first name" data-parsley-has-special-characters/>
+                        <i class="fa fa-exclamation-triangle" aria-hidden="true"></i>
+                        <i class="fa fa-check" aria-hidden="true"></i>
+                    </div>
+                </div>
+                <!-- Last Name -->
+                <div class="form-field">
+                    <label for="gc_code">Last Name *</label>
+                    <div class="field-wrapper">
+                        <input type="text" name="last_name" id="last_name" data-parsley-trigger="blur" data-parsley-required="a last name" data-parsley-has-special-characters/>
+                        <i class="fa fa-exclamation-triangle" aria-hidden="true"></i>
+                        <i class="fa fa-check" aria-hidden="true"></i>
+                    </div>
+                </div>
 				<!-- Email -->
 				<div class="form-field">
 					<label for="gc_code">Email Address *</label>
