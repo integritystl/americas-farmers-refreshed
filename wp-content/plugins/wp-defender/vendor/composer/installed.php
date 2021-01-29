30b3012b341646d2bf73458760a5c5b8cee425f9
Drew Newman <drew.newman@integritystl.com>
2021-01-29 15:31:12 -0600

    WPMU Dev with upgrades

---
+++ wp-content/plugins/wp-defender/vendor/composer/installed.php
@@ -0,0 +1,117 @@
+<?php return array (
+  'root' =>
+  array (
+    'pretty_version' => 'dev-release/2.4.6',
+    'version' => 'dev-release/2.4.6',
+    'aliases' =>
+    array (
+    ),
+    'reference' => '6a98e2e1673132bee30ecbf95b98ecdc7f588841',
+    'name' => 'incsub/wp-defender',
+  ),
+  'versions' =>
+  array (
+    'container-interop/container-interop' =>
+    array (
+      'pretty_version' => '1.2.0',
+      'version' => '1.2.0.0',
+      'aliases' =>
+      array (
+      ),
+      'reference' => '79cbf1341c22ec75643d841642dd5d6acd83bdb8',
+    ),
+    'container-interop/container-interop-implementation' =>
+    array (
+      'provided' =>
+      array (
+        0 => '^1.0',
+      ),
+    ),
+    'gettext/gettext' =>
+    array (
+      'pretty_version' => 'v4.8.3',
+      'version' => '4.8.3.0',
+      'aliases' =>
+      array (
+      ),
+      'reference' => '57ff4fb16647e78e80a5909fe3c190f1c3110321',
+    ),
+    'gettext/languages' =>
+    array (
+      'pretty_version' => '2.6.0',
+      'version' => '2.6.0.0',
+      'aliases' =>
+      array (
+      ),
+      'reference' => '38ea0482f649e0802e475f0ed19fa993bcb7a618',
+    ),
+    'incsub/wp-defender' =>
+    array (
+      'pretty_version' => 'dev-release/2.4.6',
+      'version' => 'dev-release/2.4.6',
+      'aliases' =>
+      array (
+      ),
+      'reference' => '6a98e2e1673132bee30ecbf95b98ecdc7f588841',
+    ),
+    'mnapoli/php-di' =>
+    array (
+      'replaced' =>
+      array (
+        0 => '*',
+      ),
+    ),
+    'php-di/invoker' =>
+    array (
+      'pretty_version' => '1.3.3',
+      'version' => '1.3.3.0',
+      'aliases' =>
+      array (
+      ),
+      'reference' => '1f4ca63b9abc66109e53b255e465d0ddb5c2e3f7',
+    ),
+    'php-di/php-di' =>
+    array (
+      'pretty_version' => '5.4.6',
+      'version' => '5.4.6.0',
+      'aliases' =>
+      array (
+      ),
+      'reference' => '3f9255659595f3e289f473778bb6c51aa72abbbd',
+    ),
+    'php-di/phpdoc-reader' =>
+    array (
+      'pretty_version' => '2.2.1',
+      'version' => '2.2.1.0',
+      'aliases' =>
+      array (
+      ),
+      'reference' => '66daff34cbd2627740ffec9469ffbac9f8c8185c',
+    ),
+    'psr/container' =>
+    array (
+      'pretty_version' => '1.0.0',
+      'version' => '1.0.0.0',
+      'aliases' =>
+      array (
+      ),
+      'reference' => 'b7ce3b176482dbbc1245ebf52b181af44c2cf55f',
+    ),
+    'psr/container-implementation' =>
+    array (
+      'provided' =>
+      array (
+        0 => '^1.0',
+      ),
+    ),
+    'vlucas/valitron' =>
+    array (
+      'pretty_version' => 'v1.4.9',
+      'version' => '1.4.9.0',
+      'aliases' =>
+      array (
+      ),
+      'reference' => '81515dcc951e1f636a1a18ece2f4154dfa123438',
+    ),
+  ),
+);
